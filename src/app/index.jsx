fe9e57d73cf13bafe52380961dc581a9db11c772
egrieshaber <eric.j.grieshaber@gmail.com>
2019-05-14 19:57:56 -0400

    now displaying groups names in dashboard

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,9 +2,11 @@ import { store } from './store'
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Dashboard } from './components/Dashboard'
+import { Main } from './components/Main'
+// import { Dashboard } from './components/Dashboard'
+
 
 ReactDOM.render (
-  <Dashboard />,
+  <Main />,
   document.getElementById("app")
 );
